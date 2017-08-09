36679d87ab681ffb01dd1cef4d5e23780d5ed37f
Erik Drysén <erik.drysen@outlook.com>
2017-08-09 23:15:28 +0200

    Add project files.

---
+++ MasterDetail_Demo/MasterDetail_Demo/ViewModels/MenuViewModel.cs
@@ -0,0 +1,23 @@
+﻿using MasterDetail_Demo.Models;
+using MasterDetail_Demo.Views;
+using System.Collections.ObjectModel;
+
+namespace MasterDetail_Demo.ViewModels
+{
+    public class MenuViewModel
+    {
+        public ObservableCollection<MenuItemModel> MenuItems { get; }
+
+        public MenuViewModel()
+        {
+            MenuItems = new ObservableCollection<MenuItemModel>();
+            LoadMenuItems();
+        }
+
+        private void LoadMenuItems()
+        {
+            MenuItems.Add(new MenuItemModel { Id = 0, Title = "TestPage1", IconSource = "icon.png", TargetType = typeof(Page1) });
+            MenuItems.Add(new MenuItemModel { Id = 1, Title = "TestPage2", IconSource = "icon.png", TargetType = typeof(Page2) });
+        }
+    }
+}
---
+++ MasterDetail_Demo/MasterDetail_Demo/Views/MainMasterPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MasterDetail_Demo.Models;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MasterDetail_Demo.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainMasterPage : MasterDetailPage
+    {
+        public MainMasterPage()
+        {
+            InitializeComponent();
+            mainMenuPage.ListView.ItemSelected += ListView_ItemSelected;
+        }
+
+        private void ListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var item = e.SelectedItem as MenuItemModel;
+            if (item == null)
+                return;
+
+            var page = (Page)Activator.CreateInstance(item.TargetType);
+
+            Detail = new NavigationPage(page);
+            IsPresented = false;
+
+            mainMenuPage.ListView.SelectedItem = null;
+        }
+    }
+}
